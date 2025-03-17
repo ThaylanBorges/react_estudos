579e291860423d7a5ae116636404f65b4b706ac8
ThaylanBorges <taylan-150963@hotmail.com>
2025-03-17 16:17:51 -0300

    Configurando o projeto

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Tasks from "./components/Tasks";
+import AddTask from "./components/AddTask";
+
+function App() {
+  return (
+    <>
+      <h1>Gerenciador de Tarefas</h1>
+      <Tasks></Tasks>
+      <AddTask></AddTask>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/AddTask.jsx
@@ -0,0 +1,9 @@
+function AddTask() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline bg-sky-800">Add Task</h1>
+    </>
+  );
+}
+
+export default AddTask;
